0661850f1a86aa0c4c82b87817e4342d9096c2d9
Shivam Sharma <shivji171106@gmail.com>
2026-09-09 15:15:42 +0000

    step 1  of hajime no ippo

---
+++ first_basic/operations.js
@@ -0,0 +1,30 @@
+let score ="340"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" = Number
+// "33abs" = NaN
+// "Undefined" = NaN
+// "null" = 0
+
+
+let kathmandu = 1
+let bulgaria = Boolean(kathmandu)
+
+console.log( bulgaria);
+
+// 1 = true 
+// 0 = false
+// 2,3,4,45 = true
+// "shivam" =  true
+// "" = false
+
+let daiku ="1234as"
+
+let majamaja = String(daiku)
+
+console.log(typeof majamaja);
