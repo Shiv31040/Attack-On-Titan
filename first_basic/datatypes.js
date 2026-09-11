5246f6b2b1449aec8d35c3a7024a372945736e30
Shivam Sharma <shivji171106@gmail.com>
2026-09-11 19:54:49 +0530

    day3(part-2)

--- first_basic/datatypes.js
+++ first_basic/datatypes.js
@@ -1,39 +1,62 @@
-//datatypes 
-/*
+// //datatypes 
+// /*
 
-Primitve:-
-string ,number,boolean,null ,undefined,symbol , bigInt
-*/
-let bigNumber = 2365081640715604605n
+// Primitve:-
+// string ,number,boolean,null ,undefined,symbol , bigInt
+// */
+// let bigNumber = 2365081640715604605n
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
-/*
-Non-Premitive:-
-Array ,objects ,functions
-*/
+// /*
+// Non-Premitive:-
+// Array ,objects ,functions
+// */
 
-const maleHeroes= ["Batman","IronMan"];
-console.log(typeof maleHeroes);
+// const maleHeroes= ["Batman","IronMan"];
+// console.log(typeof maleHeroes);
 
-let myObject={
-    name:"Batman",
-    age:44,
-}
+// let myObject={
+//     name:"Batman",
+//     age:44,
+// }
 
-console.log(typeof myObject);
+// console.log(typeof myObject);
 
-let myFunctions = function(){
-    console.log("I am the Best");
+// let myFunctions = function(){
+//     console.log("I am the Best");
 
     
+// }
+// console.log(typeof myFunctions);
+
+
+
+
+// let playStation = Symbol(2347)
+// let xBox = Symbol(2347)
+// console.log(playStation);
+// console.log(playStation === xBox);
+
+//********************************Stack(Primitve),Heap(Non-Primitive)**************************************
+
+let shiv1 = "ShahrukKhan"
+let shiv2 = shiv1
+
+shiv2 ="SalmanKhan"
+
+console.log(shiv1);
+console.log(shiv2);
+
+
+let realObject = {
+    name: "ShahrukKhan",
+    email:"srk@gmail.com",
 }
-console.log(typeof myFunctions);
 
+let realObject2 = realObject
 
+realObject2.email ="sml@gmail.com"
 
-
-let playStation = Symbol(2347)
-let xBox = Symbol(2347)
-console.log(playStation);
-console.log(playStation === xBox);
+console.log(realObject2.email);
+console.log(realObject.email);
