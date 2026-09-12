591440d8f0e05fa96ab2b90e92a407069e8fa8d5
Shivam Sharma <shivji171106@gmail.com>
2026-09-12 11:38:35 +0530

    day 4 (part-1)

---
+++ first_basic/nums_and_maths.js
@@ -0,0 +1,17 @@
+// let mard = new Number(216.2343)
+// console.log(mard.toPrecision(5));
+
+
+// const ravan = 1000000000
+// console.log(ravan.toLocaleString('en-In'));
+
+//*****************************Maths**********************************
+
+// console.log(Math.abs(-12345));
+// console.log(Math.random());
+// console.log(Math.random()*10+1);
+// console.log(Math.floor(Math.random()*10+1));
+
+let min = 1
+let max = 6
+console.log(Math.floor(Math.random()*(max-min + 1) + min));
