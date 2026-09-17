cfc70e025ff3a67717f63307fdee8e5aa9eed3f4
Shivam Sharma <shivji171106@gmail.com>
2026-09-17 19:00:26 +0530

    Basics complete

--- third_basic/4_iiife.js
+++ third_basic/4_iiife.js
@@ -2,10 +2,10 @@
 /*
 Global scope ke pollution se bachane ke kuch specific functions ke liye hum iife ka istemaal karte hai*/
 
-(function jaadu(){//named iife
-    console.log("Database connected");
-})();
+// (function jaadu(){//named iife
+//     console.log("Database connected");
+// })();
 
-((name)=>{//unmaed iife
-    console.log(`Database is connected again, ${name}`);
-})("Shivam");
+// ((name)=>{//unmaed iife
+//     console.log(`Database is connected again, ${name}`);
+// // })("Shivam");
